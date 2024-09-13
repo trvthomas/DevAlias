ce8faf387eb6bfd854e975a2ff3b9899bbfcd451
Thomas Rodriguez <thomasrodriguez@outlook.com>
2024-09-13 12:28:48 -0700

    Pending add more names and results UI

--- include/names.js
+++ include/names.js
@@ -1,14 +1,14 @@
 const faveLanguageNouns = {
-    "html": ["a", "b"],
-    "python": ["c", "d"],
-    "js": ["e", "f"],
-    "java": ["g", "h"],
-    "other": ["i", "j"],
-    "none": ["k", "l", "m"]
+    "html": ["Blueprint Engineer", "", "", "", "", "", "", "", "", ""],
+    "python": ["Scripter", "Snakemaster", "Constructor", "Pythoneer", "Automator", "Algorithmist", "", "", "", ""],
+    "js": ["Looper", "DOMinator", "Debugger", "", "", "", "", "", "", ""],
+    "java": ["Methodologist", "Abstractionist", "Executor", "Compiler Wizard", "", "", "", "", "", ""],
+    "other": ["Code Explorer", "Tech Pioneer", "Debug Wizard", "Algorithm Crafter", "", "", "", "", "", ""],
+    "none": ["Thinker", "Ideologist", "Creator", "Strategist", "Dreamer", "", "", "", "", ""]
 }
 
 const skillLevelAdjectives = {
-    "beginner": ["n", "o"],
-    "intermediate": ["p", "q"],
-    "advanced": ["r", "s"]
+    "beginner": ["Curious", "Fresh", "Learning", "Inquisitive", "Adventurous", "Exploratory", "Enthusiastic", "Eager", "Newbie", "Aspiring", "Dedicated", "Evolving", "Entry-Level", "Novice", "Experimental", "Creative", "Fresh", "Emerging", "Problem-Solving", "Ambitious"],
+    "intermediate": ["Skilled", "Confident", "Adaptive", "Resourceful", "Versatile", "Competent", "Tech-Savvy", "Productive", "Collaborative", "Capable", "Innovative", "Analytical", "Efficient", "Dynamic", "Scalable", "Thoughtful", "Structured", "Proactive", "Methodical", "Insightful"],
+    "advanced": ["Masterful", "Expert", "Visionary", "Strategic", "Pioneering", "Optimized", "Trailblazing", "Elite", "Seasoned", "Accomplished", "Leading", "Revolutionary", "Efficient", "Tactical", "Influential", "Advanced", "Senior", "Legendary", "Unstoppable", "Dominant"]
 }
