bdec412f260e3a178550a09c9fc45aa1989d154f
Thomas Rodriguez <46304934+trvthomas@users.noreply.github.com>
2024-09-12 22:31:17 -0700

    First beta version

---
+++ include/names.js
@@ -0,0 +1,14 @@
+const faveLanguageNouns = {
+    "html": ["a", "b"],
+    "python": ["c", "d"],
+    "js": ["e", "f"],
+    "java": ["g", "h"],
+    "other": ["i", "j"],
+    "none": ["k", "l", "m"]
+}
+
+const skillLevelAdjectives = {
+    "beginner": ["n", "o"],
+    "intermediate": ["p", "q"],
+    "advanced": ["r", "s"]
+}
