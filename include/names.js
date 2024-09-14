9f63cc8c32dde78baaff8865b82fe69154f3e04f
Thomas Rodriguez <thomasrodriguez@outlook.com>
2024-09-13 21:16:56 -0700

    Version 1.0

--- include/names.js
+++ include/names.js
@@ -1,14 +1,20 @@
 const faveLanguageNouns = {
-    "html": ["Blueprint Engineer", "", "", "", "", "", "", "", "", ""],
-    "python": ["Scripter", "Snakemaster", "Constructor", "Pythoneer", "Automator", "Algorithmist", "", "", "", ""],
-    "js": ["Looper", "DOMinator", "Debugger", "", "", "", "", "", "", ""],
-    "java": ["Methodologist", "Abstractionist", "Executor", "Compiler Wizard", "", "", "", "", "", ""],
-    "other": ["Code Explorer", "Tech Pioneer", "Debug Wizard", "Algorithm Crafter", "", "", "", "", "", ""],
-    "none": ["Thinker", "Ideologist", "Creator", "Strategist", "Dreamer", "", "", "", "", ""]
+    "html": ["Blueprint Engineer", "Designer", "Formatter", "Structurer", "Architect", "Markup Wizard", "UI Shaper", "Template Designer", "Grid Master", "Mapper"],
+    "python": ["Scripter", "Snakemaster", "Constructor", "Pythoneer", "Automator", "Algorithmist", "Analyst", "Data Tamer", "Engine Builder", "Optimizer"],
+    "js": ["Looper", "DOMinator", "Debugger", "Builder", "Code Artisan", "Guru", "Magician", "Renderer", "Taskmaster", "Maestro"],
+    "java": ["Methodologist", "Abstractionist", "Executor", "Compiler Wizard", "Platformer", "Frameworker", "Backend Alchemist", "System Builder", "Class Architect", "Process Handler"],
+    "other": ["Code Explorer", "Tech ", "Debug Maestro", "Algorithm Crafter", "Techie", "Pioneer", "Generalist", "Developer", "Coder", "Innovator"],
+    "none": ["Thinker", "Ideologist", "Creator", "Strategist", "Dreamer", "Seeker", "Futurist", "Visionary", "Tech Enthusiast", "Curiosity Seeker"]
 }
 
 const skillLevelAdjectives = {
     "beginner": ["Curious", "Fresh", "Learning", "Inquisitive", "Adventurous", "Exploratory", "Enthusiastic", "Eager", "Newbie", "Aspiring", "Dedicated", "Evolving", "Entry-Level", "Novice", "Experimental", "Creative", "Fresh", "Emerging", "Problem-Solving", "Ambitious"],
     "intermediate": ["Skilled", "Confident", "Adaptive", "Resourceful", "Versatile", "Competent", "Tech-Savvy", "Productive", "Collaborative", "Capable", "Innovative", "Analytical", "Efficient", "Dynamic", "Scalable", "Thoughtful", "Structured", "Proactive", "Methodical", "Insightful"],
     "advanced": ["Masterful", "Expert", "Visionary", "Strategic", "Pioneering", "Optimized", "Trailblazing", "Elite", "Seasoned", "Accomplished", "Leading", "Revolutionary", "Efficient", "Tactical", "Influential", "Advanced", "Senior", "Legendary", "Unstoppable", "Dominant"]
+}
+
+const invitingMessages = {
+    "beginner": ["Coding can be tough, but you don't have to go it alone. Jump into our Discord for tips, tricks, and more", "Ready lo level up you skills? Our club is the perfect place to start, join our Discord now!", "You're just getting started. Joing our club and let's grow together!", "Your tech journey starts here! Join our Discord and our awesome community", "Like your new DevAlias? Join our club and start builing something incredible!"],
+    "intermediate": ["{{language}} is powerful, and so are you! Join our club for more tips, tricks, news, and more", "Coding in {{language}} is always better with friends. Join our club and level up together!", "Enhance your skills with us! Join our Discord and collaborate with other skilled coders", "Your coding skills are growing fast. Join our club and share your knowledge (or learn even more)!", "Every great tech-passionate needs a crew. Join our community on Discord for more fun!"],
+    "advanced": ["You're a coding pro! Join our club and help others--or take on a challenge yourself!", "Got a passion for {{language}}? Join our club and build something awesome with us!", "With your skills you can inspire others! Join our Discord and be part of this thriving community", "Coding or not, come share your expertise and passions with our friendly community on Discord!", "You've got the name, now get in the game. Join our club and start building impressive things!"]
 }
